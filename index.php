d4f3e74eac2704b203c3a4e1db6c4fc337eefd10
devpahita <devpachita@gmail.com>
2025-08-26 20:38:08 +0000

    Daily Push

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!doctype html>
+<html lang="es">
+
+  <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- Referencias para Datatables -->
+      <script src= "https://code.jquery.com/jquery-3.7.1.js"></script>
+      <script src= "https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
+      <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css" integrity="sha384-Xbg45MqvDIk1e563NLpGEulpX6AvL404DP+/iCgW9eFa2BqztiwTexswJo2jLMue" crossorigin="anonymous">
+      
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+      <title>Taller Front</title>
+  </head>
+
+    <body>
+
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+
+    <div class="d-flex justify-content-center">
+      <div class="card mb-3" style="max-width: 940px;">
+        <div class="row g-0">
+          <div class="col-md-4 d-flex align-items-center justify-content-center p-3">
+            <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png" 
+                class="img-fluid rounded-start" 
+                alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet</h5>
+              <p class="card-text">
+                From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. 
+                Wear facing inward to be bestowed with love and abundance, or outward for protection.
+              </p>
+              <a href="#" class="btn btn-primary">
+                <i class="fa-solid fa-eye"></i> Ver Detalles
+              </a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    </body>
+
+</html>
