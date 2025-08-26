d539adc8c82cbaa8ae8366ec2f7f1ef5e1e67a61
devpahita <devpachita@gmail.com>
2025-08-26 20:01:56 +0000

    index

---
+++ homePage/index.php
@@ -0,0 +1,57 @@
+
+<!doctype html>
+<html lang="es">
+
+
+  
+  <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- Referencias para Datatables -->
+      <!-- <script src= "https://code.jquery.com/jquery-3.7.1.js"></script>
+      <script src= "https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
+      <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css"> -->
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css" integrity="sha384-Xbg45MqvDIk1e563NLpGEulpX6AvL404DP+/iCgW9eFa2BqztiwTexswJo2jLMue" crossorigin="anonymous">
+      
+
+      <title>FrontEnd en HTML y PHP</title>
+  </head>
+
+    <body>
+
+      <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+
+      <div class="card mb-3" style="max-width: 540px;">
+      <div class="row g-0">
+          <div class="col-md-4">
+            <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="#" class="btn btn-primary">Ver Detalles</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </body>
+
+</html>
